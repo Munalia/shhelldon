6aa715384a5290af1c084ec4842c985951141c3f
Munalia <69430348+Munalia@users.noreply.github.com>
2020-11-21 11:54:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,18 +1,20 @@
 class Translation(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """A Simple File Renamer Bot With Permanent Thumbnail support!💯
 
-<b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>"""
+<b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>
+🚨 PRON video🔞 Links gives you PERMANENT BAN 🚨
+       ┈┈┈••💙✿❤️✿💚••┈┈┈"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @prgofficial"
-    DOWNLOAD_START = "Give Me Some Time Bruh!!(〒﹏〒)"
-    UPLOAD_START = "Hurray... Completed Downloading(;ŏ﹏ŏ)"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    DOWNLOAD_START = "<b>Downloding To My Server</b> 📥 \n<code>Please Wait... Uploding Start Soon</code>"
+    UPLOAD_START = "<b>Uploding To Telegram</b> 📤 \n<code>😁😍👍Hurray!!!</code>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @KS_RenamerBot **"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @prgofficial"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @prgofficial"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @KumarSwamiKS"
     SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Saved ✅ This Is Permanent"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
@@ -23,5 +25,6 @@ class Translation(object):
     HELP_USER = """It's not that complicated😅
     
 1. Send me any Telegram File.
-2. Reply to that message with /rename NEWNAME.EXTENSION"""
+2. Reply to that message with /rename NEWNAME.EXTENSION
+       ┈┈┈••💙✿❤️✿💚••┈┈┈"""
 
